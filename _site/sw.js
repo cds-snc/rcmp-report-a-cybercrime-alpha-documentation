2d8c08a03e921ebddf7efe7ea1f1bd9970de6b70
Daniel Tse <daniel.tse@cds-snc.ca>
2019-10-09 09:48:27 -0400

    Temp remove of research from menu.

--- _site/sw.js
+++ _site/sw.js
@@ -1,17 +1,17 @@
-importScripts( '/rcmp-report-a-cybercrime-alpha-documentation/cache-polyfill.js' );
+importScripts( '/cache-polyfill.js' );
 
 var filesToCache = [
   // root
-  '/rcmp-report-a-cybercrime-alpha-documentation/',
-  '/rcmp-report-a-cybercrime-alpha-documentation/index.html',
+  '/',
+  '/index.html',
   // css
-  '/rcmp-report-a-cybercrime-alpha-documentation/assets/css/main.css',
-  '/rcmp-report-a-cybercrime-alpha-documentation/assets/css/normalize.css',
-  '/rcmp-report-a-cybercrime-alpha-documentation/assets/css/syntax.css',
+  '/assets/css/main.css',
+  '/assets/css/normalize.css',
+  '/assets/css/syntax.css',
   // images
-  '/rcmp-report-a-cybercrime-alpha-documentation/assets/img/octocat.png',
+  '/assets/img/octocat.png',
   // pages
-  '/rcmp-report-a-cybercrime-alpha-documentation/alpha-research/','/rcmp-report-a-cybercrime-alpha-documentation/background/','/rcmp-report-a-cybercrime-alpha-documentation/contact/','/rcmp-report-a-cybercrime-alpha-documentation/home/','/rcmp-report-a-cybercrime-alpha-documentation/participate/','/rcmp-report-a-cybercrime-alpha-documentation/prototypes/','/rcmp-report-a-cybercrime-alpha-documentation/team-news/','/rcmp-report-a-cybercrime-alpha-documentation/technical-platform/','/rcmp-report-a-cybercrime-alpha-documentation/accueil/','/rcmp-report-a-cybercrime-alpha-documentation/coordonnees/','/rcmp-report-a-cybercrime-alpha-documentation/contexte/','/rcmp-report-a-cybercrime-alpha-documentation/nouvelles-dequipe/','/rcmp-report-a-cybercrime-alpha-documentation/participez/','/rcmp-report-a-cybercrime-alpha-documentation/recherche-de-la-phase-decouverte/',
+  '/alpha-research/','/background/','/contact/','/home/','/participate/','/prototypes/','/team-news/','/technical-platform/','/accueil/','/coordonnees/','/contexte/','/nouvelles-dequipe/','/participez/','/recherche-de-la-phase-decouverte/',
   // posts
   
 ];
